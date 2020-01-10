570a5723b6404138e547339072e54e7578ff6f2e
fumiyamiyazaki <fmy9611@icloud.com>
2020-01-10 22:54:02 +0900

    private

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,8 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show]
 
   def index
-    # @topics = Topic.all
+    @topics = Topic.all
   end
 
   def new
@@ -9,11 +10,20 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.create params.require(:topic).permit(:name, :text, :image)
+    @topic = Topic.create(topic_params)
     redirect_to @topic
   end
 
   def show
+    @topic = Topic.find(params[:id])
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:name, :text, :image)
+  end
+
+  def set_topic
     @topic = Topic.find(params[:id])
   end
 
