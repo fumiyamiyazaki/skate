6bb81397ae43d4301507eb5d74e5b3b3b9137ddf
fumiyamiyazaki <fmy9611@icloud.com>
2020-01-17 06:02:16 +0900

    maypage_implement3

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,6 @@ class Topic < ApplicationRecord
 
   belongs_to :user
 
+  # mount_uploader :image, ImageUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_11_074359) do
+ActiveRecord::Schema.define(version: 2020_01_16_203856) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_01_11_074359) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -49,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_01_11_074359) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
