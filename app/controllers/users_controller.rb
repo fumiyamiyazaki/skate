98673ffe39494ce71abc1000ba299ba78b25d26e
fumiyamiyazaki <fmy9611@icloud.com>
2020-02-01 03:32:39 +0900

    following-followers-view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :move_to_signup, only: [:edit, :create, :update, :following, :followers]
 
 
   def show
@@ -25,10 +26,28 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following
+    render 'show_following'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_followers'
+  end
+
   private
   
   def user_params
     params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :image)
+  end
+
+  def move_to_signup
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
 end
